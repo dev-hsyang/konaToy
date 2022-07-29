dd266e8f5f9a4920eda1eba9a5b22f11a8d0268d
hyunseung yang <dev.hsyang@gmail.com>
2022-07-29 17:02:42 +0900

    22.07.29

--- src/main/java/com/konai/hsyang/konatoy/KonaToyApplication.java
+++ src/main/java/com/konai/hsyang/konatoy/KonaToyApplication.java
@@ -2,7 +2,9 @@ package com.konai.hsyang.konatoy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class KonaToyApplication {
 
