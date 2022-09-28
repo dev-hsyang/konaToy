2186845e8ddfc3f707a51405bf278302e609d094
hyunseung yang <dev.hsyang@gmail.com>
2022-09-28 15:07:48 +0900

    add: view uploaded files

--- src/main/java/com/konai/hsyang/konatoy/file/dto/FileResponseDto.java
+++ src/main/java/com/konai/hsyang/konatoy/file/dto/FileResponseDto.java
@@ -9,12 +9,12 @@ public class FileResponseDto {
     private Long fileID;
     private Long postID;
     private String orgFileName;
-    private Long fileSize;
+    private int fileSize;
 
     public FileResponseDto(File entity) {
         this.fileID = entity.getFileID();
         this.postID = entity.getPosts().getPostsID();
         this.orgFileName = entity.getOrgfilename();
-        this.fileSize = entity.getFilesize();
+        this.fileSize = Math.round(entity.getFilesize()/1024);
     }
 }
--- src/main/java/com/konai/hsyang/konatoy/posts/controller/PostsController.java
+++ src/main/java/com/konai/hsyang/konatoy/posts/controller/PostsController.java
@@ -38,7 +38,7 @@ public class PostsController {
         model.addAttribute("author", postsService.isPostAuthor(principalDetails.getId(), postsService.postsResponseDtoFindById(id)));
         model.addAttribute("comments", commentsService.getCommentsList(principalDetails.getNickname(), id)); // 작성자인지 아닌지 판단 위해 responseDto에서 commentsList를 불러오지 않고 별도 함수 호출
         model.addAttribute("location", locationService.findByID(responseDto.getLocation().getLocationID()));
-        model.addAttribute("files", responseDto.getFiles());
+        model.addAttribute("filelist", responseDto.getFiles());
         return "posts-view";
     }
 
