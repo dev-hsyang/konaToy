d864fc834482dc62a5da01da2df3cab2641d16a8
hyunseung yang <dev.hsyang@gmail.com>
2022-10-17 15:28:20 +0900

    fixed: test code - posts-api-controller

--- src/test/java/com/konai/hsyang/konatoy/posts/controller/PostsApiControllerTest.java
+++ src/test/java/com/konai/hsyang/konatoy/posts/controller/PostsApiControllerTest.java
@@ -1,4 +1,39 @@
 package com.konai.hsyang.konatoy.posts.controller;
 
+import com.konai.hsyang.konatoy.location.service.LocationService;
+import com.konai.hsyang.konatoy.posts.service.PostsService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {PostsApiController.class})
+@WebAppConfiguration
+@AutoConfigureMockMvc
 public class PostsApiControllerTest {
+
+    @Mock
+    private PostsService postsService;
+
+    @Mock
+    private LocationService locationService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("Posts 저장 API 테스트")
+    @WithUserDetails
+    @Test
+    void saveTest(){
+
+    }
 }
--- src/test/java/com/konai/hsyang/konatoy/posts/controller/PostsControllerTest.java
+++ src/test/java/com/konai/hsyang/konatoy/posts/controller/PostsControllerTest.java
@@ -14,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
